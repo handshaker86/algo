6d2860572e12b32a876bb586b13bbcc8e831310a
handshaker86 <handshaker@sjtu.edu.cn>
2025-08-20 10:41:51 +0800

    chore: delete infer bash script

--- infer_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# show ${RUNTIME_SCRIPT_DIR}
-echo ${RUNTIME_SCRIPT_DIR}
-# enter infer workspace
-cd ${RUNTIME_SCRIPT_DIR}
-
-# write your code below
-python -u infer.py --hidden_units 64  --num_blocks 4 --num_heads 4 
